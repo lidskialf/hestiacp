ac535168c77ca4c5c0cd7a37544aad4e0044fe7e
Kristan Kenney <40798553+kristankenney@users.noreply.github.com>
2020-03-14 17:00:11 -0300

    Update jail.local on existing installs to enable recidive f2b chain

--- install/upgrade/versions/latest.sh
+++ install/upgrade/versions/latest.sh
@@ -11,3 +11,9 @@ if [ -e "/etc/exim4/exim4.conf.template" ]; then
     echo "(*) Updating exim4 configuration..."
     sed -i "s|rfc1413_query_timeout = 5s|rfc1413_query_timeout = 0s|g" /etc/exim4/exim4.conf.template
 fi
+
+# Update fail2ban jail settings to enable recidive (#716)
+if [ -e "/etc/fail2ban/jail.local" ]; then
+    echo "(*) Updating fail2ban security settings..."
+    cp -f $HESTIA_INSTALL_DIR/fail2ban/jail.local /etc/fail2ban/jail.local
+fi
